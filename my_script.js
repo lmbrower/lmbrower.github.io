e7577db81c013df7687cd824873330a0c4953332
lmbrower <loganbrower75@gmail.com>
2024-07-26 10:58:16 -0500

    added audio

--- my_script.js
+++ my_script.js
@@ -1,3 +1,9 @@
 $('#heClickedIt').click(function() {
     $('#hiddenPart').attr('hidden', false);
 });
+
+function getAJob() {
+    console.log('here');
+    let audio = new Audio('get_a_job.ogg');
+    audio.play();
+}
